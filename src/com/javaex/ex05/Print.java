6ffec3a3eadeaf3c15e038546052b2af76cc1696
lee <l_lpp@naver.com>
2024-11-07 18:02:52 +0900

    Add .gitignore

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -4,10 +4,16 @@ public class Print {
 	
 	
 	// 매소드 세개
-//	private int i;
+	private int i;
 	private String t;
 	private double o;
 	private String name;
+	
+	public point(int , int y) {
+		// 초기화
+		this.x = x;
+		this.x = y;
+	}
 	
 	
 	
