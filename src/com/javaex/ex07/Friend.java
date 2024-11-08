df6d5565693339f2cb661f856b331a6c6fd00c43
lee <l_lpp@naver.com>
2024-11-08 13:06:58 +0900

    연습문제7번

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -1,18 +1,55 @@
 package com.javaex.ex07;
 
+// 쪼개서 순서로 나열해보면
+// 친구 객제를 3개 만들어야함. 프렌드[] <배열을 써야지
+// 3번 돌아야하니까. 프렌트[3] / 총 방이 3개니까 길이는 3-1
+// 각각 네임, 나이, 학교 를 입력받아야함
+// 프렌드 객체를 생성 
+// 배열의 반복, 추가 (이걸 총 3번 돌면 됨)
+// 루프 돌기 
+// 프렌드 객체 추출하기(뽑아내기)
+// 출력코드 부르기
 public class Friend {
-
+	// 필드가 필요하지
     private String name;
     private String hp;
     private String school;
-
     
+    // 기본 생성자 만들기 
+    public Friend(String name, String hp, String school) {
+    	this.name=name;
+    	this.hp=hp;
+    	this.school=school;
+    }
+   
     // getter/setter 작성
+    public String getName() {
+    	return name;
+    }
     
+    public void setName(String name) {
+    	this.name=name;
+    }
     
+    public String getHp() {
+    	return hp;
+    }
     
+    public void setHp(String hp) {
+    	this.hp =hp;
+    }
+    
+    public String getSchool() {
+    	return school;
+    }
+    
+    public void setSchool(String school) {
+    	this.school =school;
+    }
+    
+    // 일반 메서드
     public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
+        System.out.println("이름:"+name+", 핸드폰:"+hp+", 학교:"+school);
     }
 
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -12,16 +12,39 @@ public class FriendApp {
         System.out.println("친구를 3명 등록해 주세요");
 
         
+        
         //for문 시작
         //친구정보 3명 입력 로직 --> 반복문 사용
+        for (int i=0; i<friendArray.length; ++i) {	// 3번 돌린다고
+        	// 이제 입력 받아야 해 
+        	String name, hp, school; 	
+        	
+        	System.out.println("이름: ");
+//        	name = scanner.next();
+        	name = sc.next();	// name으로 입력 받아야함 (이거 이젠 알아야한다고, 모르면 안된다고하시는데 진짜 모르겠다)
+        	System.out.println("핸드폰: ");
+        	hp=sc.next();		// hp로 받아야함 
+        	System.out.println("학교:");
+        	school=sc.next();	// school로 받아야함
+        	
+        	
+            Friend f = new Friend(name, hp, school);  // 초기화 시키는거지 이거?
+            friendArray[i] = f;		
             
-      
+            System.out.println("---------------------");
+        	
         //for문 끝
+        }			
         
         
+        
+  //-----------------------------출력---------//       
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
+        	Friend f = friendArray[i];
+        	f.showInfo();  
+        	
         }
 
         sc.close();
